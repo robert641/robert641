452f3b82deb675695f5c00e784705b0f1fe661de
RobertAlfonzo <susejrobert@gmail.com>
2024-12-20 16:03:44 -0400

    print suma mas bonito

--- pruebita-xd.py
+++ pruebita-xd.py
@@ -1,12 +1,12 @@
 def suma_de_numeros(a,b):
     c = a + b 
-    print(c)
+    print(f'la suma de los numeros es: {c}')
     
 
 
 
-a = int(input('ingrese rl primer numero: '))
-b = int(input('ingrese el segundo numero: '))
+a = int(input('ingrese rl primer numero usuario : '))
+b = int(input('ingrese el segundo numero usuario: '))
 suma_de_numeros(a,b)
     
     
