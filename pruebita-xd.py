111235ad352eb93bed4c3d93ee0d809f3a04743c
Robert <susejrobert@gmail.com>
2024-12-20 14:31:21 -0400

    Add files via upload

---
+++ pruebita-xd.py
@@ -0,0 +1,16 @@
+def suma_de_numeros(a,b):
+    c = a + b 
+    print(c)
+    
+
+
+
+a = int(input('ingrese rl primer numero: '))
+b = int(input('ingrese el segundo numero: '))
+suma_de_numeros(a,b)
+    
+    
+    
+    
+    
+    
